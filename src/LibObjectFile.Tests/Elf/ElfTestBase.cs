26db7f882135e794a44a11bda2104a1217a64432
Alexandre Mutel <alexandre_mutel@live.com>
2019-11-16 08:35:32 +0100

    Assert ReadOnly mode in the tests

--- src/LibObjectFile.Tests/Elf/ElfTestBase.cs
+++ src/LibObjectFile.Tests/Elf/ElfTestBase.cs
@@ -10,40 +10,11 @@ namespace LibObjectFile.Tests.Elf
         protected static void AssertReadElf(ElfObjectFile elf, string fileName)
         {
             AssertReadElfInternal(elf, fileName);
-            AssertReadBack(elf, fileName);
+            AssertReadBack(elf, fileName, readAsReadOnly: false);
+            AssertReadBack(elf, fileName, readAsReadOnly: true);
             AssertLsbMsb(elf, fileName);
         }
-
-        private static void AssertReadElf(ElfObjectFile elf, string fileName, bool writeFile = true, string context = null)
-        {
-            if (writeFile)
-            {
-                using (var stream = new FileStream(Path.Combine(Environment.CurrentDirectory, fileName), FileMode.Create))
-                {
-                    elf.Write(stream);
-                    stream.Flush();
-                }
-            }
-
-            var stringWriter = new StringWriter();
-            elf.Print(stringWriter);
-
-            var result = stringWriter.ToString().Replace("\r\n", "\n");
-            var readelf = LinuxUtil.ReadElf(fileName);
-            Console.WriteLine("=== Expected:");
-            Console.WriteLine(readelf);
-            Console.WriteLine("=== Result:");
-            Console.WriteLine(result);
-            if (context != null)
-            {
-                Assert.AreEqual(readelf, result, context);
-            }
-            else
-            {
-                Assert.AreEqual(readelf, result);
-            }
-        }
-
+        
         private static void AssertReadElfInternal(ElfObjectFile elf, string fileName, bool writeFile = true, string context = null)
         {
             if (writeFile)
@@ -74,30 +45,30 @@ namespace LibObjectFile.Tests.Elf
             }
         }
         
-        private static void AssertReadBack(ElfObjectFile elf, string fileName)
+        private static void AssertReadBack(ElfObjectFile elf, string fileName, bool readAsReadOnly)
         {
             ElfObjectFile newObjectFile;
 
             var filePath = Path.Combine(Environment.CurrentDirectory, fileName);
-            using (var stream = new FileStream(filePath, FileMode.Open))
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
-                newObjectFile = ElfObjectFile.Read(stream);
+                newObjectFile = ElfObjectFile.Read(stream, new ElfReaderOptions() {ReadOnly = readAsReadOnly});
+
+                Console.WriteLine();
+                Console.WriteLine("=============================================================================");
+                Console.WriteLine("readback");
+                Console.WriteLine("=============================================================================");
+                Console.WriteLine();
+
+                AssertReadElfInternal(newObjectFile, fileName, false, $"Unexpected error while reading back {fileName}");
+
+                var originalBuffer = File.ReadAllBytes(filePath);
+                var memoryStream = new MemoryStream();
+                newObjectFile.Write(memoryStream);
+                var newBuffer = memoryStream.ToArray();
+
+                Assert.AreEqual(originalBuffer, newBuffer, "Invalid binary diff between write -> (original) -> read -> write -> (new)");
             }
-
-            Console.WriteLine();
-            Console.WriteLine("=============================================================================");
-            Console.WriteLine("readback");
-            Console.WriteLine("=============================================================================");
-            Console.WriteLine();
-
-            AssertReadElfInternal(newObjectFile, fileName, false, $"Unexpected error while reading back {fileName}");
-
-            var originalBuffer = File.ReadAllBytes(filePath);
-            var memoryStream = new MemoryStream();
-            newObjectFile.Write(memoryStream);
-            var newBuffer = memoryStream.ToArray();
-
-            Assert.AreEqual(originalBuffer, newBuffer, "Invalid binary diff between write -> (original) -> read -> write -> (new)");
         }
 
         private static void AssertLsbMsb(ElfObjectFile elf, string fileName)
@@ -111,7 +82,8 @@ namespace LibObjectFile.Tests.Elf
             elf.Encoding = ElfEncoding.Msb;
             var newFileName = Path.GetFileNameWithoutExtension(fileName) + "_msb.elf";
             AssertReadElfInternal(elf, newFileName);
-            AssertReadBack(elf, newFileName);
+            AssertReadBack(elf, newFileName, readAsReadOnly: false);
+            AssertReadBack(elf, newFileName, readAsReadOnly: true);
         }
     }
 }
