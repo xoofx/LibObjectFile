d8d13a1b01b7b3fe46aab97b130614b470ae969b
Alexandre Mutel <alexandre_mutel@live.com>
2019-11-16 10:47:02 +0100

    Add license headers

--- src/LibObjectFile/Elf/ElfDecoderSwap.cs
+++ src/LibObjectFile/Elf/ElfDecoderSwap.cs
@@ -1,4 +1,8 @@
-﻿using LibObjectFile.Utils;
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license.
+// See the license.txt file in the project root for more information.
+
+using LibObjectFile.Utils;
 
 namespace LibObjectFile.Elf
 {
--- src/LibObjectFile/Utils/BinaryUtil.cs
+++ src/LibObjectFile/Utils/BinaryUtil.cs
@@ -1,4 +1,8 @@
-﻿using System.Runtime.CompilerServices;
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license.
+// See the license.txt file in the project root for more information.
+
+using System.Runtime.CompilerServices;
 
 namespace LibObjectFile.Utils
 {
