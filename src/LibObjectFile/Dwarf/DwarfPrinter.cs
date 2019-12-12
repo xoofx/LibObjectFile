53afcfb1c7756445474bdc87a97fe146ae9482b7
Alexandre Mutel <alexandre_mutel@live.com>
2019-12-12 22:44:38 +0100

    Add printer for DwarfAbbreviation

--- src/LibObjectFile/Dwarf/DwarfPrinter.cs
+++ src/LibObjectFile/Dwarf/DwarfPrinter.cs
@@ -2,16 +2,44 @@
 // This file is licensed under the BSD-Clause 2 license.
 // See the license.txt file in the project root for more information.
 
+using System;
+using System.IO;
+
 namespace LibObjectFile.Dwarf
 {
     public static class DwarfPrinter
     {
+        public static void Print(this DwarfDebugAbbrevTable abbrevTable, TextWriter writer)
+        {
+            if (writer == null) throw new ArgumentNullException(nameof(writer));
 
+            writer.WriteLine("Contents of the .debug_abbrev section:");
+            
+            foreach (var abbreviation in abbrevTable.Abbreviations)
+            {
+                Print(abbreviation, writer);
+            }
+        }
 
+        public static void Print(this DwarfAbbreviation abbreviation, TextWriter writer)
+        {
+            if (writer == null) throw new ArgumentNullException(nameof(writer));
 
+            writer.WriteLine();
 
+            writer.WriteLine($"  Number TAG (0x{abbreviation.Offset})");
 
-
-        
+            foreach (var item in abbreviation.Items)
+            {
+                writer.WriteLine($"   {item.Code}      {item.Tag}    [{(item.HasChildren ? "has children" : "no children")}]");
+                var descriptors = item.Descriptors;
+                for (int i = 0; i < descriptors.Length; i++)
+                {
+                    var descriptor = descriptors[i];
+                    writer.WriteLine($"    {descriptor.Kind.ToString(),-18} {descriptor.Form}");
+                }
+                writer.WriteLine("    DW_AT value: 0     DW_FORM value: 0");
+            }
+        }
     }
 }
