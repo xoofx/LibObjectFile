f7c9fa5319dd75b052b12df5f9286c78a94d24c3
Bruno Garcia <bruno@brunogarcia.com>
2019-12-21 23:02:42 +0100

    helper script

--- src/test-with-docker.sh
+++ src/test-with-docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export TEST_TAG_NAME=libobjectfile-tests
+
+if [[ "$(docker images -q ${TEST_TAG_NAME}:latest 2> /dev/null)" == "" ]]; then
+    docker build -t ${TEST_TAG_NAME} .
+fi
+
 # Run unit tests in Docker - See Dockerfile
-docker build -t libobjectfile-tests . \
-    && docker run libobjectfile-tests
+docker run -v `pwd`:/src --rm -it ${TEST_TAG_NAME}
+
