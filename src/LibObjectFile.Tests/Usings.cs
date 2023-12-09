13f8e6738e0ee09752962b2ebf0b13af3647194b
Alexandre Mutel <alexandre_mutel@live.com>
2023-12-09 08:09:21 +0100

    Update to .NET 8

---
+++ src/LibObjectFile.Tests/Usings.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// This file is licensed under the BSD-Clause 2 license.
+// See the license.txt file in the project root for more information.
+
+global using NUnit.Framework;
+global using Assert = NUnit.Framework.Legacy.ClassicAssert;
+global using CollectionAssert = NUnit.Framework.Legacy.CollectionAssert;
+global using StringAssert = NUnit.Framework.Legacy.StringAssert;
+global using DirectoryAssert = NUnit.Framework.Legacy.DirectoryAssert;
+global using FileAssert = NUnit.Framework.Legacy.FileAssert;
